f68300c3374b822254be302991aac557af5df944
ricoandrianto-dev <ricoandrianto@unesa.ac.id>
2026-06-16 20:13:53 +0700

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,14 +1,14 @@
 // WAJIB DIISI AGAR REALTIME BERFUNGSI DI VERCEL.
 // Ambil dari Firebase Console > Project settings > Your apps.
 // Pastikan Realtime Database sudah dibuat dan databaseURL benar.
-window.FIREBASE_CONFIG = {
-  apiKey: "ISI_API_KEY_FIREBASE",
-  authDomain: "ISI_PROJECT.firebaseapp.com",
-  databaseURL: "https://ISI_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "ISI_PROJECT",
-  storageBucket: "ISI_PROJECT.appspot.com",
-  messagingSenderId: "ISI_MESSAGING_SENDER_ID",
-  appId: "ISI_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyDjeLTdmGUdYLiGGNkxI8wIgAc4Z-RCoZY",
+  authDomain: "e-lkpd-2c898.firebaseapp.com",
+  projectId: "e-lkpd-2c898",
+  storageBucket: "e-lkpd-2c898.firebasestorage.app",
+  messagingSenderId: "621847371250",
+  appId: "1:621847371250:web:20acc9caa2323a4aaa9f6b",
+  measurementId: "G-S50HG3HPXL"
 };
 
 // Ganti classId jika ingin membuat kelas/ruang data baru tanpa menghapus data lama.
